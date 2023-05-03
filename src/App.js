a58b075c3e9dc556a05cb4b50b476881af5d666f
Fayelazona <90098873+Fayelazona@users.noreply.github.com>
2023-05-03 14:10:52 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { TodoWrapper } from './Components/TodoWrapper';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h5>FAYE CHARISSE LAZONA</h5>
+      <TodoWrapper/>
+    </div>
+  );
+}
+
+export default App;
